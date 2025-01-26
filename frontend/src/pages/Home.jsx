39259f09cb553854ac3ff6aaa81193226a85c2a8
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2025-01-26 05:22:40 +0500

    done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,8 +23,8 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            // const url = "https://deploy-mern-app-1-api.vercel.app/products";
-            const url = 'https://localhost:8080/auth/products'
+            const url = "https://deploy-mern-app-1-api.vercel.app/products";
+            // const url = 'https://localhost:8080/auth/products'
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
